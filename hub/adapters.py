fe8e36a305ef49f92d5b2b373497c928d1423ef2
manuelceb <manuelceb83@gmail.com>
2026-08-08 23:08:05 -0400

    edge cases error handling

--- hub/adapters.py
+++ hub/adapters.py
@@ -1,31 +1,32 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-
+from urllib.parse import urlencode
+from allauth.core.exceptions import ImmediateHttpResponse
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
 
 class ExistingUserAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         return False
 
 
-# users/adapters.py
+class ExistingUserSocialAccountAdapter(DefaultSocialAccountAdapter):
 
-from urllib.parse import urlencode
-
-from allauth.core.exceptions import ImmediateHttpResponse
-from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.contrib import messages
-from django.shortcuts import redirect
-from django.urls import reverse
-
-
-class ExistingUserSocialAccountAdapter(
-    DefaultSocialAccountAdapter
-):
-    """
-    Allow Google authentication only for existing IL-Hub users.
-    """
+    def pre_social_login(self, request, sociallogin):
+        """
+        Extracting picture from Google profile recevied. 
+        Storing only strictly required data, aligned with GDPR principles.
+        """
+        extra_data = sociallogin.account.extra_data
+        extra_data.pop('picture', None)
+        sociallogin.account.extra_data = extra_data
 
     def is_open_for_signup(self, request, sociallogin):
+        """
+        Allow Google authentication only for existing IL-Hub users.
+        """
+
         messages.error(
             request,
             (
@@ -46,3 +47,24 @@ class ExistingUserSocialAccountAdapter(
         raise ImmediateHttpResponse(
             redirect(login_url)
         )
+
+    def on_authentication_error(
+        self,
+        request,
+        provider,
+        error=None,
+        exception=None,
+        extra_context=None,
+    ):
+        """
+        Custom error handling when user cannot be authenticated 
+        (The user possibly refuses agreement on Google's webpage)
+        """
+        messages.error(
+            request,
+            "Google authentication could not be completed.",
+        )
+
+        raise ImmediateHttpResponse(
+            redirect("hub:login")
+         )
--- v1/urls.py
+++ v1/urls.py
@@ -5,8 +5,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # allauth
-    path('accounts/', include('allauth.urls')),
+    # restricted allauth links
+    path('accounts/', include('hub.allauth_urls')),
 
     # Hub OAuth provider endpoints
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
